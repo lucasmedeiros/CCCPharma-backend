507240f2092236745ff0b4275ab500d59db3c3bf
Luan <jadsonluan1@gmail.com>
2018-12-17 09:28:27 -0300

    Inicializa banco com conta de admin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO category(id, name, discount) VALUES (1, 'Medicamentos', 0);
 INSERT INTO category(id, name, discount) VALUES (2, 'Higiene Pessoal', 0);
 INSERT INTO category(id, name, discount) VALUES (3, 'Cosméticos', 0);
 INSERT INTO category(id, name, discount) VALUES (4, 'Alimentos', 0);
+
+INSERT INTO table_user(email, password, role) VALUES ('admin@admin.com', '$2a$10$tZ9ayU4YOrwUhJlq8gTaD.9b70/GKsgJNWTTW6RPGlCbD56/HgqCy', 'ADMIN');
